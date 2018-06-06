5b4d8a93b778fbe9b9fbd7dc3557690d3a0fd574
Jennifer Hong <jennifer.hong13@gmail.com>
2018-06-06 20:39:35 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,7 @@
-# code your #position_taken? method here!
+position_taken?
+  if board [0] == "X" || board [0] == "O"
+    "taken"
+  elsif board [0] != "X" || board [0] != "O"
+    "not taken"
+
+    
