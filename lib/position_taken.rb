ef68066f8be01b015e15673e13d7c0da05b0b060
Jennifer Hong <jennifer.hong13@gmail.com>
2018-06-06 20:43:29 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,5 @@
 def position_taken?(board, index)
-  if (board[index] == " " || board[index] == " " || board[index] == nil)
+  if (board[index] == " " || board[index] == "" || board[index] == nil)
     taken = false
   else
     taken = true
